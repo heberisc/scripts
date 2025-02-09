9e4e3594acd9dea247d543c9d0f1200443f2255d
Heber <heber.isc@gmail.com>
2025-02-09 12:30:52 -0600

    Update jm.sh

--- jm.sh
+++ jm.sh
@@ -7,11 +7,11 @@ for ((i=ip1; i<=ip2; i++)); do
     ip="$net.$i.$gw"
     # si se encuentra un segmento
     if ping -c 1 -w 1 $ip > /dev/null 2>&1; then
-        echo "Segmento activo encontrado: $net.$i.0/24"
+        echo "Segmento: $net.$i.0/24"
         # segundo for para escanear todas las IPs dentro de ese segmento
         for (( j=1; j<=254; j++ )); do
             target_ip="$net.$i.$j"
-            ping -c 1 -W 1 "$target_ip" > /dev/null 2>&1 && echo "Host activo: $target_ip"
+            ping -c 1 -W 1 "$target_ip" > /dev/null 2>&1 && echo "$target_ip"
         done
     fi
 done
